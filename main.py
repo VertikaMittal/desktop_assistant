27623c4998f1c984c301a167af70be19cdc64d71
Vertika <mittalvertika1803200@gmail.com>
2022-03-23 13:01:18 +0530

    update

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
 
         elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"Sir, the time is {strTime}")
+            speak(f"Mam, the time is {strTime}")
 
         elif 'open code' in query:
             path = '"C:\\Users\\miita\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"'
